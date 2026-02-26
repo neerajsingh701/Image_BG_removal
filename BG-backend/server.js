4fceec329df310ba66e702c113d39daa569b579c
Neeraj singh <royneeraj521@gmail.com>
2026-02-26 19:29:51 +0530

    Add files

--- BG-backend/server.js
+++ BG-backend/server.js
@@ -44,7 +44,7 @@ const startServer = async () => {
 
         await connectDB();
         const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0', () => {
             console.log(`Server is running on port ${PORT}`);
         });
 
